12e37ba97f27e2d0626e0f936b95e2b24082602f
salma abouerrimah <salmaabouerrimah0@gmail.com>
2025-01-16 14:48:54 +0100

    l'ajout d'un fichier Contact.js

---
+++ my-portfolio/src/components/Contact.js
@@ -0,0 +1,14 @@
+// components/Contact.js
+import React from "react";
+
+function Contact() {
+  return (
+    <div className="contact">
+      <h1>Contact Me</h1>
+      <p>Feel free to reach out for collaborations or inquiries:</p>
+      {/* Add contact form or details */}
+    </div>
+  );
+}
+
+export default Contact;
