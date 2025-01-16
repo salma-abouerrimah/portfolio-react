7517a3382e1432916afbfa6a3e3b234240d0179d
salma abouerrimah <salmaabouerrimah0@gmail.com>
2025-01-16 14:52:51 +0100

    l'ajout d'un fichier Resume.js

---
+++ my-portfolio/src/components/Resume.js
@@ -0,0 +1,14 @@
+// components/Resume.js
+import React from "react";
+
+function Resume() {
+  return (
+    <div className="resume">
+      <h1>My Resume</h1>
+      <p>Download my resume or explore my professional experience below:</p>
+      {/* Add resume details */}
+    </div>
+  );
+}
+
+export default Resume;
