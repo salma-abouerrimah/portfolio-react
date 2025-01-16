6ff72cbba14f314b791d3a1a590617a8a316f76e
salma abouerrimah <salmaabouerrimah0@gmail.com>
2025-01-16 14:50:38 +0100

    l'ajout d'un fichier Home.js

---
+++ my-portfolio/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome to My Portfolio</h1>
+      <p>Explore my projects, resume, and learn more about me!</p>
+    </div>
+  );
+}
+
+export default Home;
