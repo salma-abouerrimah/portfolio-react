88c19dc8ac46b213c07c7dffd76bcf1a2a7817cd
salma abouerrimah <salmaabouerrimah0@gmail.com>
2025-01-16 14:46:52 +0100

    l'ajout d'une dossier components et un ficher About.js

---
+++ my-portfolio/src/components/About.js
@@ -0,0 +1,14 @@
+// components/About.js
+import React from "react";
+
+function About() {
+  return (
+    <div className="about">
+      <h1>About Me</h1>
+      <p>Learn more about my journey as a digital developer:</p>
+      {/* Add about details */}
+    </div>
+  );
+}
+
+export default About;
