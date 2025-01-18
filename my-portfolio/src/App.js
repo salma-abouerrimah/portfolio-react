366999e1d546c67b5fa5d5d9846da0130b0f4b3b
salma abouerrimah <salmaabouerrimah0@gmail.com>
2025-01-18 15:40:26 +0100

    installation de web-vitals

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, NavLink } from "react-router-dom";
+import Home from "./components/Home";
+import Projects from "./components/Projects";
+import Resume from "./components/Resume";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <nav className="navbar">
+          <NavLink to="/" className="nav-link" end>Home</NavLink>
+          <NavLink to="/projects" className="nav-link">Projects</NavLink>
+          <NavLink to="/resume" className="nav-link">Resume</NavLink>
+          <NavLink to="/about" className="nav-link">About</NavLink>
+          <NavLink to="/contact" className="nav-link">Contact</NavLink>
+        </nav>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/resume" element={<Resume />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
